1256baf201fa622e7900acaa5004762df8fae7cd
redkorr <redkor910.97@gmail.com>
2024-02-12 14:46:36 +0100

    cr: code cleanup; added height limit on input box

--- src/app/chat/components/messageInput.tsx
+++ src/app/chat/components/messageInput.tsx
@@ -13,18 +13,26 @@ export default function MessageInput({ sendMessage }: Props) {
     }
   };
 
-  useAutosizeTextArea(textAreaRef.current, text);
+  useEffect(() => {
+    if (!textAreaRef.current) return;
+    if (textAreaRef.current.scrollHeight <= 160) {
+      textAreaRef.current.style.height = '0px';
+      const scrollHeight = textAreaRef.current.scrollHeight;
+      textAreaRef.current.style.height = scrollHeight + 'px';
+    } else {
+      textAreaRef.current.style.height = '160px';
+    }
+  }, [textAreaRef, text]);
 
   const handleSendMessage = () => {
-    if (text.trim().length > 0) {
-      sendMessage(text);
-      setText('');
-    }
+    if (text.trim().length === 0) return;
+    sendMessage(text);
+    setText('');
   };
 
   return (
     <div className='fixed bottom-0 left-0 w-full flex flex-col justify-center items-center'>
-      <div className='w-full md:w-4/5 mx-3 flex items-end gap-2 pb-5 pt-3 bg-black'>
+      <div className='w-full md:w-4/5 mx-3 flex items-end gap-2 pb-5 pt-2 px-2 bg-black'>
         <div className='rounded-3xl w-full flex items-end bg-zinc-800'>
           <textarea
             className='text-gray-100 bg-zinc-800 p-2 my-auto w-full rounded-3xl resize-none focus-visible:outline-none'
@@ -52,19 +60,6 @@ export default function MessageInput({ sendMessage }: Props) {
     </div>
   );
 }
-const useAutosizeTextArea = (
-  textAreaRef: HTMLTextAreaElement | null,
-  value: string
-) => {
-  useEffect(() => {
-    if (textAreaRef) {
-      textAreaRef.style.height = '0px';
-      const scrollHeight = textAreaRef.scrollHeight;
-      textAreaRef.style.height = scrollHeight + 'px';
-    }
-  }, [textAreaRef, value]);
-};
-
 interface Props {
-  sendMessage: (s: string) => any;
+  sendMessage: (s: string) => void;
 }
