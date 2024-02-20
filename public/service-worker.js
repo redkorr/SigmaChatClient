d03016e267835670413a6650a63f678eeaefeaa5
senbar <sendek.bartosz@gmail.com>
2024-02-20 12:30:17 +0100

    bugfix: notification click closes notification and opens sigma chat.
    I pasted random code from stackoverflow supposedly opening PWA on
    notification click.
    Apparently android also doesnt close notifications on click so it does
    that as well.
    Also fixed badge on notifications so that it uses our logo instead of
    google chrome's.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,11 @@
 async function checkClientIsVisible() {
   const windowClients = await clients.matchAll({
-    type: "window",
+    type: 'window',
     includeUncontrolled: true,
   });
 
   for (var i = 0; i < windowClients.length; i++) {
-    if (windowClients[i].visibilityState === "visible") {
+    if (windowClients[i].visibilityState === 'visible') {
       return true;
     }
   }
@@ -26,8 +26,34 @@ self.addEventListener('push', function (event) {
         const data = event.data.json();
         return self.registration.showNotification(data.title, {
           body: data.body,
-          icon: data.icon || null
+          icon: data.icon || null,
+          badge: data.icon || null
         });
       }
     }));
+});
+
+self.addEventListener('notificationclick', function (event) {
+  console.log('On notification click: ', event.notification.tag);
+  // Android doesn't close the notification when you click on it
+  // See: http://crbug.com/463146
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(
+    clients.matchAll({
+      type: 'window'
+    })
+      .then(function (clientList) {
+        for (var i = 0; i < clientList.length; i++) {
+          var client = clientList[i];
+          if (client.url == '/' && 'focus' in client)
+            return client.focus();
+        }
+        if (clients.openWindow) {
+          return clients.openWindow('/');
+        }
+      })
+  );
 });
